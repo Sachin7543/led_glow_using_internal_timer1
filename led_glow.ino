676d859d26a3e93924346e00c3e57b7b3ebd4903
Sachin Patil <56131842+Sachin7543@users.noreply.github.com>
2020-05-21 15:25:50 +0530

    led_glow_using_internal_timer1

---
+++ led_glow.ino
@@ -0,0 +1,26 @@
+#define ledPin 13
+int led_state=0;
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  // initialize timer1 
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 31249;            // compare match register 16MHz/256/2Hz
+  TCCR1B = TCCR1B |(1 << WGM12);   // CTC mode
+  TCCR1B = TCCR1B |((1 << CS12)|(1 << CS10));    // 256 prescaler 
+  TIMSK1 = TIMSK1 |(1 << OCIE1A);  // enable timer compare interrupt
+  sei();             // enable all interrupts
+
+}
+void loop()
+{
+  // your program here…
+}
+
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+  led_state=!led_state;        // toggle LED pin
+  digitalWrite(ledPin, led_state);   
+}
